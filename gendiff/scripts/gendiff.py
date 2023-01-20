98fdb18cc45da9570bfc1f899aa426f28c26f4ce
Jickx <63093696+Jickx@users.noreply.github.com>
2023-01-20 12:32:54 +0200

    step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,17 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-parser.parse_args()
+
+
+parser = argparse.ArgumentParser(
+    description='Compares two configuration files and shows a difference.'
+)
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help='set format of output')
+parsed = parser.parse_args()
 
 
 def main():
-    print("Help!!!")
+    print(parsed)
 
 
 if __name__ == "__main__":
