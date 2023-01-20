5c9768d2ea13817554937e5ce0ae55a95070c934
Jickx <63093696+Jickx@users.noreply.github.com>
2023-01-20 11:01:11 +0200

    step 1

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,10 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+parser.parse_args()
+
+
 def main():
     print("Help!!!")
 
